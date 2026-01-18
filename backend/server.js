362c4b6c17d9531719cfb84a2543fdbd04bb59d8
pulkit161001 <pulkit161001@gmail.com>
2026-01-18 20:15:11 +0530

    path changed

--- backend/server.js
+++ backend/server.js
@@ -66,7 +66,7 @@ io.on("connection", (socket) => {
 
 // Serve frontend in production
 if (process.env.NODE_ENV === "production") {
-	const frontendPath = path.join(__dirname, "../../frontend/dist");
+	const frontendPath = path.join(__dirname, "../frontend/dist");
 	app.use(express.static(frontendPath));
 	app.get("*", (req, res) => {
 		res.sendFile(path.join(frontendPath, "index.html"));
